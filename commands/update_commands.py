436e4835f4374357e8ad3aa256b1bd40e04131fe
Sebastian Jung <git.sebastian@gmx.de>
2019-11-23 19:17:08 +0100

    script writes now directly to documentation.md

--- commands/update_commands.py
+++ commands/update_commands.py
@@ -1,11 +1,16 @@
+# this script needs to be run in the /commands folder and the documentation.md file should be in /
+# command references need to be at the bottom otherwise this script will overwrite everything thats coming after these references
+# if the heading of the command references changes line 58 needs to be changed to the new heading
+
 import os
 import math
 
+
 directory_in_str = os.getcwd() + "/"
 
+# get all files in the working directory which are of type *.md (except the readme file)
 directory = os.fsencode(directory_in_str)
 files = []
-
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".md"):
@@ -17,17 +22,18 @@ for file in os.listdir(directory):
     else:
         continue
 
-
+# delete the main heading of the markdown file and write title and layout in Front Matter
 for file in files:
-    path = directory_in_str + file
-    f = open(path,"r")
+    f = open(directory_in_str + file, "r")
     filename = file.replace(".md", "")
     lines = f.read().splitlines()
     line = 0
     f.close()
-    f = open(path, "w")
+
+    # open and overwrite the command documentation
+    f = open(directory_in_str + file, "w")
     while line < len(lines):
-        if lines[line].find("# " + filename) == 0:
+        if lines[line].find("# " + filename) != -1:
             print("---", file=f)
             print("title: " + filename, file=f)
             print("layout: command", file=f)
@@ -40,9 +46,44 @@ for file in files:
             continue
 
 f.close()
-f = open(directory_in_str + "00_newtable.txt", "w")
+
+
+# write the new table to the end of the markdown file
+
+# save the old content of the documentation.md file (down to the heading '# Quick command references')
+f = open(directory_in_str + "../documentation.md", "r")
+lines = f.read().splitlines()
+line = 0
+content = []
+while line < len(lines):
+    if lines[line].find("# Quick command references") != -1:
+        line = line + 1
+        content = lines[0:line]
+        break
+    else:
+        line = line + 1
+        continue
+f.close()
+
+# open and overwrite documentation.md
+f = open(directory_in_str + "../documentation.md", "w")
+
+line = 0
+while line < len(content):
+    print(content[line], file=f)
+    line = line + 1
+
+print("", file=f)
+
+# calculates the length of the rows
 per_row = math.ceil(len(files)/4)
-files.append("")
+
+# append empty rows so we are not running out of the array length
+# maximum of length difference between the rows is 3 => append 3 empty items
+
+# sort list items alphabetical
+files.sort()
+
 files.append("")
 files.append("")
 files.append("")
@@ -53,6 +94,8 @@ row3 = files[2*per_row:3*per_row]
 row4 = files[3*per_row:4*per_row]
 
 l = 0
+
+# format table and write it out (format: command | command | command | command)
 while l < per_row:
     filename1 = row1[l].replace(".md", "")
     filename2 = row2[l].replace(".md", "")
