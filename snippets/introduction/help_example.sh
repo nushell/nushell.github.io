8eeb06c632f168ae5a345f12b1f7fb89053fedf4
JT <jonathandturner@users.noreply.github.com>
2021-06-22 19:57:37 +1200

    Update help_example.sh

--- snippets/introduction/help_example.sh
+++ snippets/introduction/help_example.sh
@@ -9,7 +9,6 @@ Subcommands:
   config set - Sets a value in the config
   config set_into - Sets a value in the config
   config clear - clear the config
-  config load - Loads the config from the path given
   config remove - Removes a value from the config
   config path - return the path to the config file
 
