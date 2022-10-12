3a3561d0fb9c10e1192cbea235e95bd4e81e6310
Yutaro Ohno <yutaro.ono.418@gmail.com>
2022-10-12 21:54:00 +0900

    Fix a sample code on the "moving around" chapter (#632)
    Remove hidden files from the result of `ls` command

--- snippets/moving_around/ls_deep_glob_example.sh
+++ snippets/moving_around/ls_deep_glob_example.sh
@@ -1,15 +1,13 @@
  ls **/*.md
-────┬───────────────────────────────────────────┬──────┬─────────┬────────────
- #  │ name                                      │ type │ size    │ modified 
-────┼───────────────────────────────────────────┼──────┼─────────┼────────────
-  0 │ .github/ISSUE_TEMPLATE/bug_report.md      │ File │   592 B │ 5 days ago 
-  1 │ .github/ISSUE_TEMPLATE/feature_request.md │ File │   595 B │ 5 days ago 
-  2 │ CODE_OF_CONDUCT.md                        │ File │  3.4 KB │ 5 days ago 
-  3 │ CONTRIBUTING.md                           │ File │   886 B │ 5 days ago 
-  4 │ README.md                                 │ File │ 15.0 KB │ 5 days ago 
-  5 │ TODO.md                                   │ File │  1.6 KB │ 5 days ago 
-  6 │ crates/nu-source/README.md                │ File │  1.7 KB │ 5 days ago 
-  7 │ docker/packaging/README.md                │ File │  1.5 KB │ 5 days ago 
-  8 │ docs/commands/README.md                   │ File │   929 B │ 5 days ago 
-  9 │ docs/commands/alias.md                    │ File │  1.7 KB │ 5 days ago 
- 10 │ docs/commands/append.md                   │ File │  1.4 KB │ 5 days ago
+───┬───────────────────────────────────────────┬──────┬─────────┬───────────
+ # │ name                                      │ type │ size    │ modified
+───┼───────────────────────────────────────────┼──────┼─────────┼───────────
+ 0 │ CODE_OF_CONDUCT.md                        │ File │  3.4 KB │ 5 days ago
+ 1 │ CONTRIBUTING.md                           │ File │   886 B │ 5 days ago
+ 2 │ README.md                                 │ File │ 15.0 KB │ 5 days ago
+ 3 │ TODO.md                                   │ File │  1.6 KB │ 5 days ago
+ 4 │ crates/nu-source/README.md                │ File │  1.7 KB │ 5 days ago
+ 5 │ docker/packaging/README.md                │ File │  1.5 KB │ 5 days ago
+ 6 │ docs/commands/README.md                   │ File │   929 B │ 5 days ago
+ 7 │ docs/commands/alias.md                    │ File │  1.7 KB │ 5 days ago
+ 8 │ docs/commands/append.md                   │ File │  1.4 KB │ 5 days ago
