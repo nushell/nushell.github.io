ac91a2b6cdbd2ba5102a0eebfddd9bca766992c2
amtoine <stevan.antoine@gmail.com>
2023-05-20 12:58:39 +0200

    refactor the "cell paths" section in a snippet

---
+++ snippets/types_of_data/cell-paths.sh
@@ -0,0 +1,7 @@
+> [{langs:[Rust JS Python], releases:60}].0 | table
+╭──────────┬────────────────╮
+│ langs    │ [list 3 items] │
+│ releases │ 60             │
+╰──────────┴────────────────╯
+> [{langs:[Rust JS Python], releases:60}].0.langs.2
+Python
