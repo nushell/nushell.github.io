f8d0993f4800460f27a9ab6dafac72ae6095be8c
Justin Ma <hustcer@outlook.com>
2024-04-06 20:06:55 +0800

    Upgrade vuepress and related plugins (#1341)
    * Update shiki for syntax highlighting
    
    * chore: Upgrade all deps, still does not work
    
    * Update deps should work now
    
    * Fix deprecated node modules
    
    * try to fix copy code plugin

--- .vuepress/client.js
+++ .vuepress/client.js
@@ -4,7 +4,7 @@
  * https://v2.vuepress.vuejs.org/advanced/cookbook/usage-of-client-config.html
  */
 
-import { defineClientConfig } from '@vuepress/client';
+import { defineClientConfig } from 'vuepress/client';
 import BlogPosts from './components/BlogPosts.vue';
 
 export default defineClientConfig({
