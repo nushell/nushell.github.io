c130aa1d542461e20137f759c88316a9a8ac281f
NotTheDr01ds <32344964+NotTheDr01ds@users.noreply.github.com>
2024-08-26 08:36:44 -0400

    Next round

--- .vuepress/configs/navbar/en.ts
+++ .vuepress/configs/navbar/en.ts
@@ -1,15 +1,17 @@
 import type { NavbarConfig } from '@vuepress/theme-default';
 
 export const navbarEn: NavbarConfig = [
+  { text: 'Get Nu!', link: '/book/installation' },
+  { text: 'Getting Started', link: '/book/getting_started' },
   {
     text: 'Documentation',
     children: [
-      { text: 'Nushell Book', link: '/book/' },
+      { text: 'The Nushell Book', link: '/book/' },
       { text: 'Command Reference', link: '/commands/' },
       { text: 'Cookbook', link: '/cookbook/' },
       { text: 'Language Reference Guide', link: '/lang-guide/' },
+      { text: 'Contributing Guide', link: '/contributor-book/' },
     ],
   },
-  { text: 'Contributing', link: '/contributor-book/' },
   { text: 'Blog', link: '/blog/' },
 ];
