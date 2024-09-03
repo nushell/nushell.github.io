23e18248580441686bb8f1071f565a9c184a6732
Douglas <32344964+NotTheDr01ds@users.noreply.github.com>
2024-09-03 10:44:23 -0400

    Dropdown for doc on main page (#1508)
    * Dropdown for doc on main page
    
    * Next round
    
    * Added 'This Week in Nu'
    
    * Remove link to TWiN until other issues are resolved

--- .vuepress/configs/navbar/en.ts
+++ .vuepress/configs/navbar/en.ts
@@ -1,10 +1,17 @@
 import type { NavbarConfig } from '@vuepress/theme-default';
 
 export const navbarEn: NavbarConfig = [
-  { text: 'Book', link: '/book/' },
-  { text: 'Commands', link: '/commands/' },
-  { text: 'Contrib', link: '/contributor-book/' },
-  { text: 'Cookbook', link: '/cookbook/' },
+  { text: 'Get Nu!', link: '/book/installation' },
+  { text: 'Getting Started', link: '/book/getting_started' },
+  {
+    text: 'Documentation',
+    children: [
+      { text: 'The Nushell Book', link: '/book/' },
+      { text: 'Command Reference', link: '/commands/' },
+      { text: 'Cookbook', link: '/cookbook/' },
+      { text: 'Language Reference Guide', link: '/lang-guide/' },
+      { text: 'Contributing Guide', link: '/contributor-book/' },
+    ],
+  },
   { text: 'Blog', link: '/blog/' },
-  { text: 'Ref', link: '/lang-guide/' },
 ];
