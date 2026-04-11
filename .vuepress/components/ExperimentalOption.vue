aee1a4298416c31565a5e5b376e85c057fefd1bb
Piepmatz <git+github@cptpiepmatz.de>
2026-04-12 00:32:15 +0200

    Release notes for `v0.112.0` (#2148)

---
+++ .vuepress/components/ExperimentalOption.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hint-container important">
+    <p class="hint-container-title">Experimental option</p>
+    <p>
+      This feature is behind an experimental option. Run Nushell with
+      <code>--experimental-option={{ option }}</code> or set before running
+      Nushell the environment variable to
+      <code>NU_EXPERIMENTAL_OPTIONS={{ option }}</code
+      >.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+code {
+  white-space: nowrap;
+}
+</style>
+
+<script setup lang="ts">
+const { option } = defineProps({
+  option: {
+    type: String,
+    required: true,
+  },
+});
+</script>
