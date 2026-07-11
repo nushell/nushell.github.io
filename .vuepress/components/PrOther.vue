7631be46513a8baaeb4faa9ec20e04794ed734d1
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-07-11 17:15:01 +0200

    added `PrOther` component to make adding other changes easier

---
+++ .vuepress/components/PrOther.vue
@@ -0,0 +1,15 @@
+<template>
+  (<a :href="prLink" target="_blank">#{{ pr }}</a
+  >)
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  pr: {
+    type: Number,
+    required: true,
+  },
+});
+
+const prLink = 'https://github.com/nushell/nushell/pull/' + props.pr;
+</script>
